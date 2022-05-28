d4c0580b075c26cc5e7ab8b1178d943a2750ea3c
aabhiiii <abhimanu.28mi@gmail.com>
2022-05-28 10:44:32 +0530

    updated ml code

--- ml_model.py
+++ ml_model.py
@@ -1,3 +1,12 @@
 
 
 print("adding all ml related code here")
+
+def model1():
+    print("adding code for ML Models")
+
+def model2():
+    print("adding code for ML Model2")
+
+def model3():
+    print("adding code for ML model3")
